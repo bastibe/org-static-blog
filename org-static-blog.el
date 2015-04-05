99236b0314acc3c23b3af828b22c72ecd2037bba
Bastian Bechtold <basti@bastibe.de>
2015-04-05 17:45:06 +0200

    factored out some customizable variables

--- org-static-blog.el
+++ org-static-blog.el
@@ -1,21 +1,26 @@
+(require 'ox-html)
 
 (defgroup org-static-blog nil
   "Settings for a static blog generator using org-mode"
   :group 'applications)
 
-(defcustom org-static-blog-publish-url "http://bastibe.de/"
+(defcustom org-static-blog-publish-url "http://example.com/"
   "URL of the blog"
+  :group 'org-static-blog)
+
+(defcustom org-static-blog-publish-title "Example.com"
+  "Title of the blog"
   :group 'org-static-blog)
 
 (defcustom org-static-blog-publish-directory "~/blog/"
   "Directory where published HTML files are stored"
   :group 'org-static-blog)
 
-(defcustom org-static-blog-posts-directory "~/Projects/blog/posts/"
+(defcustom org-static-blog-posts-directory "~/blog/posts/"
   "Directory where published ORG files are stored"
   :group 'org-static-blog)
 
-(defcustom org-static-blog-drafts-directory "~/Projects/blog/drafts/"
+(defcustom org-static-blog-drafts-directory "~/blog/drafts/"
   "Directory where unpublished ORG files are stored"
   :group 'org-static-blog)
 
@@ -33,6 +38,18 @@
 
 (defcustom org-static-blog-rss-file "rss.xml"
   "File name of the RSS feed."
+  :group 'org-static-blog)
+
+(defcustom org-static-blog-page-header ""
+  "HTML to put in the <head> of each page."
+  :group 'org-static-blog)
+
+(defcustom org-static-blog-page-preamble ""
+  "HTML to put before the content of each page."
+  :group 'org-static-blog)
+
+(defcustom org-static-blog-page-postamble ""
+  "HTML to put after the content of each page."
   :group 'org-static-blog)
 
 (defun org-static-blog-publish ()
@@ -99,8 +116,8 @@
 <rss version=\"2.0\">
 <channel>
   <title>Bastibe</title>
-  <description>Basti's Scratchpad on the Internet</description>
-  <link>http://bastibe.de</link>
+  <description>" org-static-blog-title "</description>
+  <link>" org-static-blog-publish-url "</link>
   <lastBuildDate>" (format-time-string "%a, %d %b %Y %H:%M:%S %z" (current-time)) "</lastBuildDate>\n")
      (dolist (entry (sort rss-entries (lambda (x y) (time-less-p (car y) (car x)))))
        (insert (cdr entry)))
@@ -141,54 +158,25 @@ as a communication channel."
 \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
 <html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">
 <head>
-<title>" title "</title>
-<meta  http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />
-<meta  name=\"author\" content=\"Bastian Bechtold\" />
-<link rel=\"alternate\" type=\"appliation/rss+xml\"
-                href=\"http://bastibe.de/rss.xml\"
-                title=\"RSS feed for bastibe.de\">
-          <link href='http://fonts.googleapis.com/css?family=Roboto&subset=latin' rel='stylesheet' type='text/css'>
-          <link href='http://fonts.googleapis.com/css?family=Ubuntu+Mono' rel='stylesheet' type='text/css'>
-          <link href= \"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />
-          <link rel=\"icon\" href=\"static/favicon.ico\">
-          <link rel=\"apple-touch-icon-precomposed\" href=\"static/favicon-152.png\">
-          <link rel=\"msapplication-TitleImage\" href=\"static/favicon-144.png\">
-          <link rel=\"msapplication-TitleColor\" href=\"#0141ff\">
-          <title>Basti's Scratchpad on the Internet</title>
-          <meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\" />
-          <meta name=\"viewport\" content=\"initial-scale=1,width=device-width,minimum-scale=1\">
-</head>
+<meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />
+<link rel=\"alternate\"
+      type=\"appliation/rss+xml\"
+      href=\"" org-static-blog-publish-url org-static-blog-rss-file "\"
+      title=\"RSS feed for " org-static-blog-publish-url "\">
+<title>" title "</title>"
+org-static-blog-page-header
+"</head>
 <body>
-<div id=\"preamble\" class=\"status\">
-<div class=\"header\">
-              <a href=\"http://bastibe.de\">Basti's Scratchpad on the Internet</a>
-              <div class=\"sitelinks\">
-                  <a href=\"http://alpha.app.net/bastibe\">alpha.app.net</a>  | <a href=\"http://github.com/bastibe\">Github</a>
-              </div>
-          </div>
-</div>
+<div id=\"preamble\" class=\"status\">"
+org-static-blog-page-preamble
+"</div>
 <div id=\"content\">
 <h1 class=\"title\">" title "</h1>\n"
 contents
 "</div>
-<div id=\"postamble\" class=\"status\">
-<div id=\"archive\"><a href=\"archive.html\">Other posts</a></div>
-              <div id=\"disqus_thread\"></div>
-              <script type=\"text/javascript\">
-              var disqus_shortname = 'bastibe';
-              (function() {
-                var dsq = document.createElement('script');
-                dsq.type = 'text/javascript';
-                dsq.async = true;
-                dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-                  })();
-              </script>
-              <noscript>Please enable JavaScript to view the
-                  <a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>
-              <a href=\"http://disqus.com\" class=\"dsq-brlink\">comments powered by <span class=\"logo-disqus\">Disqus</span></a>
-<center><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a><br /><span xmlns:dct=\"http://purl.org/dc/terms/\" href=\"http://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">bastibe.de</span> by <a xmlns:cc=\"http://creativecommons.org/ns#\" href=\"http://bastibe.de\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Bastian Bechtold</a> is licensed under a <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/\">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</center>
-</div>
+<div id=\"postamble\" class=\"status\">"
+org-static-blog-page-postamble
+"</div>
 </body>
 </html>")))
 
@@ -213,3 +201,48 @@ as a communication channel."
   <link>" url "</link>
   <pubDate>" (org-timestamp-format (car (plist-get info :date)) "%a, %d %b %Y %H:%M:%S %z") "</pubDate>
 </item>\n")))
+
+(setq org-static-blog-publish-url "http://bastibe.de/")
+(setq org-static-blog-publish-directory "~/blog/")
+(setq org-static-blog-posts-directory "~/Projects/blog/posts/")
+(setq org-static-blog-drafts-directory "~/Projects/blog/drafts/")
+
+(setq org-static-blog-page-header
+"<meta  name=\"author\" content=\"Bastian Bechtold\" />
+<link href='http://fonts.googleapis.com/css?family=Roboto&subset=latin' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu+Mono' rel='stylesheet' type='text/css'>
+<link href= \"static/style.css\" rel=\"stylesheet\" type=\"text/css\" />
+<link rel=\"icon\" href=\"static/favicon.ico\">
+<link rel=\"apple-touch-icon-precomposed\" href=\"static/favicon-152.png\">
+<link rel=\"msapplication-TitleImage\" href=\"static/favicon-144.png\">
+<link rel=\"msapplication-TitleColor\" href=\"#0141ff\">
+<meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\" />
+<meta name=\"viewport\" content=\"initial-scale=1,width=device-width,minimum-scale=1\">")
+
+(setq org-static-blog-page-preamble
+"<div class=\"header\">
+  <a href=\"http://bastibe.de\">Basti's Scratchpad on the Internet</a>
+  <div class=\"sitelinks\">
+    <a href=\"http://alpha.app.net/bastibe\">alpha.app.net</a> | <a href=\"http://github.com/bastibe\">Github</a>
+  </div>
+</div>")
+
+(setq org-static-blog-page-postamble
+"<div id=\"archive\">
+  <a href=\"archive.html\">Other posts</a>
+</div>
+<div id=\"disqus_thread\"></div>
+<script type=\"text/javascript\">
+  var disqus_shortname = 'bastibe';
+  (function() {
+    var dsq = document.createElement('script');
+    dsq.type = 'text/javascript';
+    dsq.async = true;
+    dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+  })();
+</script>
+<noscript>Please enable JavaScript to view the
+<a href=\"http://disqus.com/?ref_noscript\">comments powered by Disqus.</a></noscript>
+<a href=\"http://disqus.com\" class=\"dsq-brlink\">comments powered by <span class=\"logo-disqus\">Disqus</span></a>
+<center><a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/\"><img alt=\"Creative Commons License\" style=\"border-width:0\" src=\"http://i.creativecommons.org/l/by-sa/3.0/88x31.png\" /></a><br /><span xmlns:dct=\"http://purl.org/dc/terms/\" href=\"http://purl.org/dc/dcmitype/Text\" property=\"dct:title\" rel=\"dct:type\">bastibe.de</span> by <a xmlns:cc=\"http://creativecommons.org/ns#\" href=\"http://bastibe.de\" property=\"cc:attributionName\" rel=\"cc:attributionURL\">Bastian Bechtold</a> is licensed under a <a rel=\"license\" href=\"http://creativecommons.org/licenses/by-sa/3.0/\">Creative Commons Attribution-ShareAlike 3.0 Unported License</a>.</center>")
