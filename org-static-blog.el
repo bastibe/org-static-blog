734dd263cf79e4d5a0077f8b5ce344ea45bf7f3d
Bastian Bechtold <basti@bastibe.de>
2021-03-05 22:19:15 +0100

    increments version number and updates changelog

--- org-static-blog.el
+++ org-static-blog.el
@@ -6,7 +6,7 @@
 ;; luhuaei, zngguvnf, Qiantan Hong, Jonas Bernoulli, Théo Jacquin,
 ;; K. Scarlet, zsxh
 ;; URL: https://github.com/bastibe/org-static-blog
-;; Version: 1.4.0
+;; Version: 1.5.0
 ;; Package-Requires: ((emacs "24.3"))
 
 ;;; Commentary:
@@ -44,7 +44,7 @@
 
 (defgroup org-static-blog nil
   "Settings for a static blog generator using org-mode"
-  :version "1.4.0"
+  :version "1.5.0"
   :group 'applications)
 
 (defcustom org-static-blog-publish-url "https://example.com/"
