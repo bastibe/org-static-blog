0b2fe63966605dab0e891f22e1a91ac5f058ecd6
James Endres Howell <james@endres-howell.org>
2024-08-17 23:32:42 -0400

    Add an example config

---
+++ emfed-config-example.el
@@ -0,0 +1,8 @@
+(setq org-static-blog-emfed-comments-block 
+      "<div id=\"comments\"><h2>Comments<h2></div>
+       <a class=\"mastodon-thread\" target=\"_blank\"
+          href=\"https://emacs.ch/@jameshowell/%s\"
+          data-toot-id=\"%s\"
+          data-exclude-post=\"true\"
+          >Turn on JavaScript to view comments, or follow this link to the Mastodon thread</a>
+       <p>You can <a target=\"_blank\" href=\"https://emacs.ch/@jameshowell/%s\">add a comment on Mastodon</a>.</p>")
